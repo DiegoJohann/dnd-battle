cdb65a33fd76bdaf2458aaa120c0f69000907857
DiegoJohann <diego9.roberto7@gmail.com>
2026-08-16 17:18:00 -0300

    :lipstick: ajusta responsividade para telas mobile e reorganiza arquivos de tema de estilos

--- src/app/components/battle-toolbar/battle-toolbar.ts
+++ src/app/components/battle-toolbar/battle-toolbar.ts
@@ -1,5 +1,12 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FlameIcon, FolderOpenIcon, LucideAngularModule, PlusIcon, Trash2Icon } from 'lucide-angular';
+import { Component, ElementRef, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import {
+    FlameIcon,
+    FolderOpenIcon,
+    LucideAngularModule,
+    MenuIcon,
+    PlusIcon,
+    Trash2Icon,
+} from 'lucide-angular';
 import { TranslatePipe } from '@ngx-translate/core';
 import { SupportedLanguage } from '../../core/i18n/i18n';
 
@@ -7,7 +14,7 @@ import { SupportedLanguage } from '../../core/i18n/i18n';
     selector: 'app-battle-toolbar',
     imports: [LucideAngularModule, TranslatePipe],
     templateUrl: './battle-toolbar.html',
-    styleUrl: './battle-toolbar.scss'
+    styleUrl: './battle-toolbar.scss',
 })
 export class BattleToolbarComponent {
     @Input({ required: true }) currentLanguage!: SupportedLanguage;
@@ -23,6 +30,26 @@ export class BattleToolbarComponent {
 
     protected readonly FlameIcon = FlameIcon;
     protected readonly FolderOpenIcon = FolderOpenIcon;
+    protected readonly MenuIcon = MenuIcon;
     protected readonly PlusIcon = PlusIcon;
     protected readonly Trash2Icon = Trash2Icon;
+
+    mobileMenuOpen = false;
+
+    constructor(private elementRef: ElementRef) {}
+
+    toggleMobileMenu(): void {
+        this.mobileMenuOpen = !this.mobileMenuOpen;
+    }
+
+    closeMobileMenu(): void {
+        this.mobileMenuOpen = false;
+    }
+
+    @HostListener('document:click', ['$event'])
+    onDocumentClick(event: MouseEvent): void {
+        if (this.mobileMenuOpen && !this.elementRef.nativeElement.contains(event.target)) {
+            this.mobileMenuOpen = false;
+        }
+    }
 }
